ce0afe858fd831185029b8249044b51eae868aeb
a7xsuri <leonardofilipposurico@gmail.com>
2023-06-18 18:48:14 +0200

    correction on the function

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const verbs = ['sei', 'vai', 'continua', 'pensi', 'giochi', 'vedi', 'ascolti']
 const attributes = ['bello', 'coglione', 'così', 'qui', 'ora', 'domani', 'drogato', 'intelligente']
 
 const generatorNumber = (verbs, attributes) => {
-    const phraseLength = Math.floor(Math.random() * 4) + 1;
+    let phraseLength = Math.floor(Math.random() * 4);
+    let randomAttributes = []
     const randomVerb = Math.floor(Math.random() * verbs.length) + 1;
-    const randomAttributes = Math.floor(Math.random() * attributes.length) + 1;
-    return (phraseLength, randomVerb, randomAttributes)
+    for(let i = 0; i<phraseLength-1; i++){
+        randomAttributes.push(Math.floor(Math.random() * attributes.length) + 1)
+    }
+    
+    return generatePhrase(randomVerb, randomAttributes)
 }
 
-generatorNumber(verbs, attributes)
+generatorNumber(verbs,attributes)
