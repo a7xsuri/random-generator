dfac3f90ad8c0df5700df542dcb90b6d78bbe3b6
a7xsuri <leonardofilipposurico@gmail.com>
2023-06-18 19:01:39 +0200

    generator finished

--- main.js
+++ main.js
@@ -3,13 +3,25 @@ const attributes = ['bello', 'coglione', 'così', 'qui', 'ora', 'domani', 'droga
 
 const generatorNumber = (verbs, attributes) => {
     let phraseLength = Math.floor(Math.random() * 4);
-    let randomAttributes = []
-    const randomVerb = Math.floor(Math.random() * verbs.length) + 1;
-    for(let i = 0; i<phraseLength-1; i++){
-        randomAttributes.push(Math.floor(Math.random() * attributes.length) + 1)
+    if(phraseLength === 0){
+        phraseLength++
     }
-    
+    let randomAttributes = []
+    const randomVerb = Math.floor(Math.random() * verbs.length);
+    for(let i = 0; i<phraseLength; i++){
+        randomAttributes.push(Math.floor(Math.random() * attributes.length))
+    }
     return generatePhrase(randomVerb, randomAttributes)
+}
+
+
+const generatePhrase = (verbIndex, attr) => {
+    let phrase  = 'Tu ' + verbs[verbIndex]
+    console.log(attr)
+    for(let j = 0; j<attr.length;j++){
+        phrase = phrase + ' ' + attributes[attr[j]]
+    }
+    return console.log(phrase)
 }
 
 generatorNumber(verbs,attributes)
