55b59e38837fd96f930cdc52728752006bf452ef
a7xsuri <leonardofilipposurico@gmail.com>
2023-06-18 18:31:23 +0200

    create genereta number

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const verbs = ['sei', 'vai', 'continua', 'pensi', 'giochi', 'vedi', 'ascolti']
 const attributes = ['bello', 'coglione', 'così', 'qui', 'ora', 'domani', 'drogato', 'intelligente']
+
+const generatorNumber = (verbs, attributes) => {
+    const phraseLength = Math.floor(Math.random() * 4) + 1;
+    const randomVerb = Math.floor(Math.random() * verbs.length) + 1;
+    const randomAttributes = Math.floor(Math.random() * attributes.length) + 1;
+    return (phraseLength, randomVerb, randomAttributes)
+}
+
+generatorNumber(verbs, attributes)
